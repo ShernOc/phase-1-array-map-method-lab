fcadd7de2b5e099d8697a085d16286028ac4cd86
Sherlyne <sherlynea1994@hotmail.com>
2024-07-25 19:53:27 +0300

    map() method

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// map() used to perform an action on each element in an Array and gather the results in a new Array
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +13,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// split the words split(), then use CharAt(0) returns the first character to an uppercase, selects the first words, and joins()
+const titleCased = ()=> {
+return tutorials.map((words)=> words.split(' ').map(words=>words.charAt(0).toUpperCase()+ words.slice(1)).join(' ') );
+}; 
+
+/* Another way
+split the words split(), then use CharAt(0) returns the first character to an uppercase, selects the first words, and joins()
+const titleCased = text => text.split(' ').map(words=>words.charAt(0).toUpperCase()+ words.slice(1)).join(' '); 
+
+ We are now going to create the new array map()
+
+const titledCasedtutorial= tutorials.map(titleCased); 
+
+console.log(titledCasedtutorial); */
+
+
+
+
